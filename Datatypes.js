fb1e3699f6b6114ddf60a854d51faedbf99c59ce
Sonu Prajapati <sonuprajapati83497@gmail.com>
2025-11-13 10:25:29 +0530

    Fixed nested git issue and added all JS files

---
+++ Datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as morden version
+
+//alert("hello world");  // we are using nodejs, not browser
+
+console.log(3
+    +
+    3); //code readability should be high
+
+console.log("sonu");
+
+
+let name = "sonu";
+let age = 21;
+isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint => larger than 2 to power 53
+//let bigNumber = 1234567890123456789012345678901234567890n;
+//string => "" , '' , ``
+//boolean => true/false
+//null => standalone value
+//undefined => variable not assigned value
+//symbol => unique identifier
---
+++ variables.js
@@ -0,0 +1,26 @@
+const accountId = 123456789;
+let accountEmail = "sonu@gmail.com";
+var accountPassword  = "12345";
+accountCity = "Gwalior"; // global variable
+
+let accountState; // undefined
+
+// accountId = 2 // not allowed
+
+accountEmail = "sonu@.com"
+accountPassword = "12345655"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+/*
+prefered not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
