7ce2b6ec9f815ed14ff25f35b70930192ec18ef7
Sonu Prajapati <sonuprajapati83497@gmail.com>
2025-11-14 18:40:31 +0530

    Maths and Number in JavaScript

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+console.log(Math.abs(-4));
+
+// console.log(Math.round(4.3));
+
+// console.log(Math.ceil(4.3));
+
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(3,4,5,6,7));
+
+// console.log(Math.max(3,4,5,6,7));
