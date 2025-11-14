44b2612d024389c0c7d67c39f5cab6716cac2817
Sonu Prajapati <sonuprajapati83497@gmail.com>
2025-11-14 18:53:27 +0530

    update

--- nums_and_maths.js
+++ nums_and_maths.js
@@ -1,21 +1,21 @@
 // *************************NUMBERS*************************
 
-// const score = 400;
-// console.log(score);
+const score = 400;
+console.log(score);
 
 
-// const balance = new Number(100);
-// console.log(balance);
+const balance = new Number(100);
+console.log(balance);
 
-// console.log(balance.toString().length);
+console.log(balance.toString().length);
 
-// console.log(balance.toFixed(2));
+console.log(balance.toFixed(2));
 
-// const otherNumber = 23.8966;
-// console.log(otherNumber.toPrecision(3));
+const otherNumber = 23.8966;
+console.log(otherNumber.toPrecision(3));
 
-// const hundreds = 100000;
-// console.log(hundreds.toLocaleString("en-IN"));
+const hundreds = 100000;
+console.log(hundreds.toLocaleString("en-IN"));
 
 
 
@@ -24,21 +24,21 @@
 Math
 console.log(Math);
 
-// console.log(Math.abs(-4));
+console.log(Math.abs(-4));
 
-// console.log(Math.round(4.3));
+console.log(Math.round(4.3));
 
-// console.log(Math.ceil(4.3));
+console.log(Math.ceil(4.3));
 
-// console.log(Math.floor(4.9));
+console.log(Math.floor(4.9));
 
-// console.log(Math.min(3,4,5,6,7));
+console.log(Math.min(3,4,5,6,7));
 
-// console.log(Math.max(3,4,5,6,7));
+console.log(Math.max(3,4,5,6,7));
 
-// console.log(Math.random());
+console.log(Math.random());
 
-// console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
 
 console.log(Math.floor(Math.random()*10)+1);
 
